85e678ab43140af98d929cabe1ac02c87c6c922f
James Skemp <strivinglife@gmail.com>
2018-08-26 13:38:35 -0500

    Add new Hello container for the application
    Uses current solution in an issue to resolve TypeScript errors.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,20 @@ import * as ReactDOM from 'react-dom';
 //import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Hello from './components/Hello';
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+//import Hello from './components/Hello';
 //import Hello from './components/StatefulHello';
+
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { EnthusiasmAction } from './actions/index'
+import { StoreState } from './types/index';
+
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript'
+},);
 
 /*
 ReactDOM.render(
@@ -13,7 +25,10 @@ ReactDOM.render(
 );
 */
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  //<Hello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
