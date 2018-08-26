ccd3a48f945144f0357c7aeb8e30f160970933a7
James Skemp <strivinglife@gmail.com>
2018-08-26 11:50:06 -0500

    Add new Hello component
    Writes out a name and exclamation marks based upon an optional enthusiasm level.

---
+++ src/components/Hello.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+
+export interface Props {
+	name: string;
+	enthusiasmLevel?: number;
+}
+
+function Hello({ name, enthusiasmLevel = 1}: Props) {
+	if (enthusiasmLevel <= 0) {
+		throw new Error('You could be a little more enthusiastic. :D');
+	}
+
+	return (
+		<div className="hello">
+			<div className="greeting">
+				Hello {name + getExclamationMarks(enthusiasmLevel)}
+			</div>
+		</div>
+	);
+}
+
+export default Hello;
+
+function getExclamationMarks(numChars: number) {
+	return Array(numChars + 1).join('!');
+}
+
+// Could also have been written out as the following, if we needed to track state:
+/*
+class Hello extends React.Component<Props, object> {
+  render() {
+    const { name, enthusiasmLevel = 1 } = this.props;
+
+    if (enthusiasmLevel <= 0) {
+      throw new Error('You could be a little more enthusiastic. :D');
+    }
+
+    return (
+      <div className="hello">
+        <div className="greeting">
+          Hello {name + getExclamationMarks(enthusiasmLevel)}
+        </div>
+      </div>
+    );
+  }
+}
+*/
